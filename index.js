7d6733317713a2dff6f1c421e197387d5cee0a93
Veeru Natarajan <veeru@yahoo-inc.com>
2014-12-04 20:00:49 +0000

    http get

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("HELLO WORLD");
+var http = require('http');
+
+http.createServer(function(request, response) {
+
+    response.setHeader('Connection', 'Transfer-Encoding');
+    response.setHeader('Content-Type', 'text/html; charset=utf-8');
+    response.setHeader('Transfer-Encoding', 'chunked');
+
+    response.write('hello');
+
+    setTimeout(function() {
+        response.write(' world!');
+        response.end();
+    }, 10000);
+
+}).listen(80);
