7c3273d9b30a3d283206500eefbc4e4ca7acdd42
Veera Natarajan <veeru@yahoo-inc.com>
2015-09-24 18:00:42 -0700

    Adding zookeeper client

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var http = require('http');
+var zookeeper = require('node-zookeeper-client');
+var client = zookeeper.createClient('zk101.iris.ne1.yahoo.com:2181');
+var path = '/prod/services';
 
 http.createServer(function(request, response) {
 
@@ -12,5 +15,37 @@ http.createServer(function(request, response) {
         response.write(' world!');
         response.end();
     }, 10000);
+	
+client.connect();
 
 }).listen(80);
+
+
+
+function listChildren(client, path) {
+    client.getChildren(
+        path,
+        function (event) {
+            response.write('Got watcher event: %s', event);
+            listChildren(client, path);
+        },
+        function (error, children, stat) {
+            if (error) {
+                console.log(
+                    'Failed to list children of %s due to: %s.',
+                    path,
+                    error
+                );
+                return;
+            }
+
+            response.write('Children of %s are: %j.', path, children);
+        }
+    );
+}
+
+client.once('connected', function () {
+    response.write('Connected to ZooKeeper.');
+    listChildren(client, path);
+});
+
