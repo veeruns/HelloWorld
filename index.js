89a4a274f8c63e7ee50ec98e04682b732a85ff25
Veera Natarajan <veeru@yahoo-inc.com>
2015-10-08 15:58:04 -0700

    Changing stuff

--- index.js
+++ index.js
@@ -1,42 +1,4 @@
 var http = require('http');
-var zookeeper = require('node-zookeeper-client');
-var client = zookeeper.createClient('zk101.iris.ne1.yahoo.com:2181');
 var path = '/prod/services';
-
-http.createServer(function(request, response) {
-
-    response.setHeader('Connection', 'Transfer-Encoding');
-    response.setHeader('Content-Type', 'text/html; charset=utf-8');
-    client.connect();
-    client.once('connected', function () {
-       console.log('Connected to ZooKeeper.');
-       var data=listChildren(client,path);
-       console.log("Children are %s",data);
-    });
-    response.write("Data");
-    response.end();	
-
-}).listen(80);
-
-
-
-function listChildren(client, path) {
-    client.getChildren(
-        path,
-        function (error, children, stat) {
-            if (error) {
-                  console.log(
-                    'Failed to list children of %s due to: %s.',
-                    path,
-                    error
-                );
-                return;
-            }
-            console.log('Children of %s are: %j.', path, children);
-            return children;
-		
-        }
-    );
-}
 
 
