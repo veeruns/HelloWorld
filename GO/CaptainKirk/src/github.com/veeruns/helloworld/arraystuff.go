4f8d2eb9ab3a1528d3c51f9db2aa66b664acfa7f
Veeru Natarajan <veeruns@yahoo.com>
2017-05-22 13:51:53 -0700

    Adding array stuff

---
+++ GO/CaptainKirk/src/github.com/veeruns/helloworld/arraystuff.go
@@ -0,0 +1,16 @@
+package main
+import (
+    "fmt"
+)
+
+func arrayaddition([]int) int {
+return 0
+}
+
+func main (){
+  x := []int{1,2,3,4}
+  x=append(x,5)
+  for i:=0;i<len(x);i++ {
+  fmt.Printf("%d\n",x[i])
+}
+}
