a265ae1cdb17137bfd70fc9a805f8a84bee417e3
Veeru Natarajan <veeruns@yahoo.com>
2017-05-23 11:35:59 -0700

    Using printf

--- switchandtime.go
+++ switchandtime.go
@@ -3,8 +3,9 @@ import (
  "fmt"
  "time"
 )
-
+//WhatDay returns Whatday it is weekday or weekend
 func WhatDay()string {
+  fmt.Printf("Testing Testing Testing")
   switch time.Now().Weekday() {
   case time.Saturday,time.Sunday :
       return "Weekend"
