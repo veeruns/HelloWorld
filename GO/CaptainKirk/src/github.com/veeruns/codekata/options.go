f4d482fea0e2829f554263b412b2229aee2b7df0
Veeru Natarajan <veeruns@yahoo.com>
2017-05-24 22:23:02 -0700

    Codekata options

---
+++ GO/CaptainKirk/src/github.com/veeruns/codekata/options.go
@@ -0,0 +1,14 @@
+package codekata
+
+
+import (
+"fmt"
+"flag"
+)
+
+func Flagoperation(){
+  Logfile=flag.String("logfile","default.log","Log filename to write")
+  Loglevel=flag.String("loglevel","INFO","Log level")
+  Concurrency=flag.int(concurrency,"2","Number of threads")
+  flag.Parse()
+}
