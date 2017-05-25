8720437618f987d800b4b4e35b85991911cb7462
Veeru Natarajan <veeruns@yahoo.com>
2017-05-24 21:35:25 -0700

    Codekata package

---
+++ GO/CaptainKirk/src/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/veeruns/helloworld"
+)
+
+func main() {
+	helloworld.Arraystuff()
+	helloworld.Hashprint()
+	helloworld.Ifthenelse()
+	s := helloworld.Whatnumber(0)
+	fmt.Printf("Returned %s\n", s)
+	x := []int{1, 2, 3, 4, 5}
+	i := helloworld.Addarray(x)
+	fmt.Printf("Addition of array %d\n", i)
+	w := helloworld.WhatDay()
+	fmt.Printf("Its a %s\n", w)
+	q := helloworld.Whatami(true)
+	fmt.Printf("Whatami returns %s\n", q)
+
+	fmt.Printf("Whatami returns %s\n", helloworld.Whatami(5))
+	fmt.Printf("Whatami returns %s\n", helloworld.Whatami("test"))
+	num := helloworld.Addnumbers(1, 2, 3)
+	fmt.Printf("Addition is %d\n", num)
+	helloworld.Circlestuff()
+	helloworld.PrintRandom()
+
+}
