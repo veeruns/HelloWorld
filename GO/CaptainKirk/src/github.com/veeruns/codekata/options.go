36f5e1321f528b3960662ec12cd60795a6ec2b5b
Veeru Natarajan <veeruns@yahoo.com>
2017-05-24 22:26:02 -0700

    Fixing linter errors

--- GO/CaptainKirk/src/github.com/veeruns/codekata/options.go
+++ GO/CaptainKirk/src/github.com/veeruns/codekata/options.go
@@ -9,6 +9,7 @@ import (
 func Flagoperation(){
   Logfile:=flag.String("logfile","default.log","Log filename to write")
   Loglevel:=flag.String("loglevel","INFO","Log level")
-  Concurrency:=flag.int(concurrency,"2","Number of threads")
+  Concurrency:=flag.Int("concurrency","2","Number of threads")
+  fmt.Printf("Done\n")
   flag.Parse()
 }
