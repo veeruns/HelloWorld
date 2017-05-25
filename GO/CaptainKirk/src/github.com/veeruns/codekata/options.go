958b2dbde53150595d0d22d52d3c7e714a6f312a
Veeru Natarajan <veeruns@yahoo.com>
2017-05-24 22:33:35 -0700

    Making it global variable

--- GO/CaptainKirk/src/github.com/veeruns/codekata/options.go
+++ GO/CaptainKirk/src/github.com/veeruns/codekata/options.go
@@ -5,13 +5,15 @@ import (
 "fmt"
 "flag"
 )
-
+var Logfile string
+var Loglevel string
+var Concurrency int
 func Flagoperation(){
-  var Logfile string
+
   flag.StringVar(&Logfile,"logfile","default.log","Log filename to write")
-  var Loglevel string
+
   flag.StringVar(&Loglevel,"loglevel","INFO","Log level")
-  var Concurrency int
+  
   flag.IntVar(&Concurrency,"concurrency",2,"Number of threads")
   fmt.Printf("Done\n")
   flag.Parse()
