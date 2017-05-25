118650f5dfd5cc602a950c2f79ab954cf6948cd2
Veeru Natarajan <veeruns@yahoo.com>
2017-05-24 22:24:18 -0700

    Changing stuff

--- GO/CaptainKirk/src/github.com/veeruns/codekata/options.go
+++ GO/CaptainKirk/src/github.com/veeruns/codekata/options.go
@@ -7,8 +7,8 @@ import (
 )
 
 func Flagoperation(){
-  Logfile=flag.String("logfile","default.log","Log filename to write")
-  Loglevel=flag.String("loglevel","INFO","Log level")
-  Concurrency=flag.int(concurrency,"2","Number of threads")
+  Logfile:=flag.String("logfile","default.log","Log filename to write")
+  Loglevel:=flag.String("loglevel","INFO","Log level")
+  Concurrency:=flag.int(concurrency,"2","Number of threads")
   flag.Parse()
 }
