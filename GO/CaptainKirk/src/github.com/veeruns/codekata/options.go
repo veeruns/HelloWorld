bff93bdbc5c0ab2225fa5d814d9b53ec19a0918d
Veeru Natarajan <veeruns@yahoo.com>
2017-05-25 10:07:05 -0700

    Some go stuff

--- GO/CaptainKirk/src/github.com/veeruns/codekata/options.go
+++ GO/CaptainKirk/src/github.com/veeruns/codekata/options.go
@@ -5,11 +5,11 @@ import (
 "fmt"
 "flag"
 )
-type  Options struct {
-  logfile string
-  loglevel string
-  concurrency int
-  timeout int
+var Options struct {
+  Logfile string
+  Loglevel string
+  Concurrency int
+  Timeout int
 }
 /*
 var Logfile string
@@ -19,13 +19,14 @@ var Timeout int
 */
 func Flagoperation(){
 
-  flag.StringVar(&Options.logfile,"logfile","default.log","Log filename to write")
 
-  flag.StringVar(&Options.loglevel,"loglevel","INFO","Log level")
+  flag.StringVar(&Options.Logfile,"logfile","default.log","Log filename to write")
 
-  flag.IntVar(&Options.concurrency,"concurrency",2,"Number of threads")
+  flag.StringVar(&Options.Loglevel,"loglevel","INFO","Log level")
 
-  flag.IntVar(&Options.timeout,"timeout",2000,"Default timeout for any operations")
+  flag.IntVar(&Options.Concurrency,"concurrency",2,"Number of threads")
+
+  flag.IntVar(&Options.Timeout,"timeout",2000,"Default timeout for any operations")
   fmt.Printf("Done\n")
   flag.Parse()
 }
