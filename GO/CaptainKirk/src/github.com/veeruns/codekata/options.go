637b24e572c188abed4d0ff31b4f3265dd7785e1
Veeru Natarajan <veeruns@yahoo.com>
2017-05-24 22:29:13 -0700

    Trying

--- GO/CaptainKirk/src/github.com/veeruns/codekata/options.go
+++ GO/CaptainKirk/src/github.com/veeruns/codekata/options.go
@@ -7,9 +7,9 @@ import (
 )
 
 func Flagoperation(){
-  var Logfile string =flag.String("logfile","default.log","Log filename to write")
-  var Loglevel string =flag.String("loglevel","INFO","Log level")
-  var Concurrency Int =flag.Int("concurrency",2,"Number of threads")
+  var *Logfile string = flag.String("logfile","default.log","Log filename to write")
+  var *Loglevel string =flag.String("loglevel","INFO","Log level")
+  var *Concurrency Int =flag.Int("concurrency",2,"Number of threads")
   fmt.Printf("Done\n")
   flag.Parse()
 }
