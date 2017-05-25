603a89dc78ea2f21ac67a74d11d62a43231b3502
Veeru Natarajan <veeruns@yahoo.com>
2017-05-24 22:32:15 -0700

    Good lord

--- GO/CaptainKirk/src/github.com/veeruns/codekata/options.go
+++ GO/CaptainKirk/src/github.com/veeruns/codekata/options.go
@@ -11,7 +11,7 @@ func Flagoperation(){
   flag.StringVar(&Logfile,"logfile","default.log","Log filename to write")
   var Loglevel string
   flag.StringVar(&Loglevel,"loglevel","INFO","Log level")
-  var Concurrency Int
+  var Concurrency int
   flag.IntVar(&Concurrency,"concurrency",2,"Number of threads")
   fmt.Printf("Done\n")
   flag.Parse()
